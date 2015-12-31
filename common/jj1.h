202221d4abf449806cc17168311af32efc686e13
bbsekiya <brian210003@gmail.com>
2015-12-30 18:20:17 -0800

    first jj-01 commit

---
+++ common/jj1.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class JJ1
+{
+  public:
+    JJ1(int n) : m_N(n) {}
+    virtual ~JJ1() {}
+
+    template <typename T>
+    void add(T&& param)
+    {
+	m.push_back(std::forward<T>(param);
+    }
+
+
+  private:
+    template <typename T>
+    std::vector<T> m;
+
+};
